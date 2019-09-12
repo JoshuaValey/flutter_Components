27d06bb7923dcd671e0cefcb9bde1c06758d83e8
Joshua Valey <j97valey@gmail.com>
2019-09-12 13:09:01 -0600

    CircleAvatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnfg2zZ8q_M2zzL75YXxzK3UG5-ScivOvvtmf2ZlmsUPKNudqsDg'),
+              radius: 22.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('J', style: TextStyle(color: Colors.white)),
+              backgroundColor: Colors.orange,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          fadeInDuration: Duration(milliseconds: 200),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(
+              'https://prodimage.images-bn.com/pimages/9781975328146_p0_v1_s550x406.jpg'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
