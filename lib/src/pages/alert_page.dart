b33afbda4a0a0dcb44ae595c66f5a23a5847153c
Joshua Valey <j97valey@gmail.com>
2019-09-11 22:18:56 -0600

    Widget - Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -16,6 +16,49 @@ class AlertPage extends StatelessWidget {
           Navigator.pop(context);
         },
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta(context),
+        ),
+      ),
     );
+  }
+
+  Widget _mostrarAlerta(BuildContext context) {
+    showDialog(
+        context: context,
+        //Cerrar el alerta tocando fuera de ella.
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text("Titulo"),
+            content: Column(
+              //Se adapta al contenido
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Este es el contenido de la caja de la alerta'),
+                FlutterLogo(
+                  size: 100.0,
+                )
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        });
   }
 }
