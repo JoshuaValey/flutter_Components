426e75942ad22c06f0108f12b861cda6c92e522d
Joshua Valey <j97valey@gmail.com>
2019-09-15 08:57:20 -0600

    Infinite Scroll and scroll controller

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -6,7 +6,27 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4, 5, 10, 20, 30, 40, 50];
+  ScrollController _scrollController = new ScrollController();
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener(() {
+      //Propiedades -> POSICION ACTUAL EN PIXELES Y MAXIMO DE PIXELES.
+
+      //Si la posicion actual es de n pixeles
+      //y el scroll maximo es de esos n pixeles
+      //-> Estamos al final de la lista
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregar10();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +40,7 @@ class _ListPageState extends State<ListPage> {
   Widget _crearLista() {
     //Renderiza los elementos segun sea necesario.
     return ListView.builder(
+      controller: _scrollController,
       itemBuilder: (BuildContext context, int index) {
         final imagen = _listaNumeros[index];
         return FadeInImage(
@@ -30,6 +51,15 @@ class _ListPageState extends State<ListPage> {
       //Cuantos elementos tiene la lista
       itemCount: _listaNumeros.length,
     );
+  }
+
+  ///Este metodo agrega 10 img a la lista
+  void _agregar10() {
+    for (var i = 1; i <= 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+      setState(() {});
+    }
   }
 }
 //https://picsum.photos/id/935/500/300
